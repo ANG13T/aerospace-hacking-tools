c5f3b88ef66daa24509bbd33ceb5eb0af9e7545a
angelina-tsuboi <angelina.t1832@gmail.com>
2023-06-13 23:01:37 -0700

    init change

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -225,20 +225,7 @@ function Sidenav({ color, brandName, routes, ...rest }) {
           },
         })}
       >
-        <SidenavCard color={color} />
-        <VuiBox mt={2}>
-          <VuiButton
-            component="a"
-            href="https://creative-tim.com/product/vision-ui-dashboard-pro-react"
-            target="_blank"
-            rel="noreferrer"
-            variant="gradient"
-            color={color}
-            fullWidth
-          >
-            Upgrade to PRO
-          </VuiButton>
-        </VuiBox>
+  
       </VuiBox>
     </SidenavRoot>
   );
