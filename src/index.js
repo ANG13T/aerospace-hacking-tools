bc9ee6ca9c0b8462daacc6e6c920d7ee500d0034
angelina-tsuboi <angelina.t1832@gmail.com>
2023-06-18 11:08:13 -0700

    feat: update files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-/*!
 
-=========================================================
-* Vision UI Free React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/vision-ui-free-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-react/blob/master LICENSE.md)
-
-* Design and Coded by Simmmple & Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the software.
-
-*/
 
 import React from "react";
 import { createRoot} from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
+import 'index.css';
 
 // Vision UI Dashboard React Context Provider
 import { VisionUIControllerProvider } from "context";
