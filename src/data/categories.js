06857b422ae513e6999b631ba682a343f1835d0c
angelina-tsuboi <angelina.t1832@gmail.com>
2023-06-25 11:41:00 -0700

    feat: organize categories

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,26 +1,26 @@
 const categories = [
-    {tag: "All Tools", emoji: "💫"},
-    {tag: "Avionics", emoji: "✈️"},
-    {tag: "Satellites", emoji: "🛰️"},
-    {tag: "Drones", emoji: "🚁"},
-    {tag: "UAV", emoji: "🛸"},
-    {tag: "Ground Control", emoji: "🎮"},
-    {tag: "Planetary Defense", emoji: "🌍"},
-    {tag: "Digital Forensics", emoji: "🔍"},
-    {tag: "Radio", emoji: "📻"},
-    {tag: "Spacecraft", emoji: "🚀"},
-    {tag: "Star Trackers", emoji: "✨"},
-    {tag: "Astrodynamics", emoji: "☄️"},
-    {tag: "Simulation", emoji: "🎮"},
-    {tag: "Mission Design", emoji: "✏️"},
-    {tag: "Mission Control", emoji: "🎮"},
-    {tag: "Ground Stations", emoji: "📡"},
-    {tag: "Planets", emoji: "🪐"},
-    {tag: "Launches", emoji: "🎆"},
-    {tag: "Earth", emoji: "🌍"},
-    {tag: "Astronomy", emoji: "🔭"},
-    {tag: "OSINT", emoji: "🕵️‍♂️"}
-  ]
+  {tag: "All Tools", emoji: "💫"},
+  {tag: "Astrodynamics", emoji: "☄️"},
+  {tag: "Astronomy", emoji: "🔭"},
+  {tag: "Avionics", emoji: "✈️"},
+  {tag: "Digital Forensics", emoji: "🔍"},
+  {tag: "Drones", emoji: "🚁"},
+  {tag: "Earth", emoji: "🌍"},
+  {tag: "Ground Control", emoji: "🎮"},
+  {tag: "Ground Stations", emoji: "📡"},
+  {tag: "Launches", emoji: "🎆"},
+  {tag: "Mission Control", emoji: "🎮"},
+  {tag: "Mission Design", emoji: "✏️"},
+  {tag: "OSINT", emoji: "🕵️‍♂️"},
+  {tag: "Planetary Defense", emoji: "🌍"},
+  {tag: "Planets", emoji: "🪐"},
+  {tag: "Radio", emoji: "📻"},
+  {tag: "Satellites", emoji: "🛰️"},
+  {tag: "Simulation", emoji: "🎮"},
+  {tag: "Spacecraft", emoji: "🚀"},
+  {tag: "Star Trackers", emoji: "✨"},
+  {tag: "UAV", emoji: "🛸"}
+];
   
   
 export default categories;
